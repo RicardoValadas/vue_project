1ee2649168c3019604549a596f4e52d25cce557e
Ricardo <ricardo.valadas07@gmail.com>
2021-11-12 08:33:45 +0100

    first

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="btn">
+    <button :style="{ background:color }">{{ text }}</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Button",
+  props: {
+    text: {
+      type: String,
+      default: "Error",
+    },
+    color:{
+      type: String,
+      default: "grey"
+    }
+  },
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="nav">
+    <router-link to="/">Home</router-link>
+  </div>
+  <router-view></router-view>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Header",
+}
+</script>
+
+<style scoped>
+.nav {
+  display: flex;
+  gap: 10px;
+  justify-content: center;
+  margin: 20px auto 60px auto;
+  padding: 30px;
+  background-color: #DFF2D8;
+}
+
+.nav a {
+  text-decoration: none;
+  color: #6CAE75;
+  font-size: 25px;
+  transition: 1s;
+}
+
+.nav a:hover {
+  color: black;
+  transform: rotate(3deg);
+}
+</style>
